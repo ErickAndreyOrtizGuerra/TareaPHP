5aae06ed2e9f2e69e5ff81d54638b3975003a225
erickortiz <guerraerick54@gmail.com>
2024-02-22 20:16:51 -0600

    primo o no

--- tarea.php
+++ tarea.php
@@ -1,16 +1,30 @@
 <?php
+// Funcion para verificar si un numero es primo
+function esPrimo($numero) {
+    // Un numero primo es mayor que 1
+    if ($numero <= 1) {
+        return false;
+    }
 
-// Definir el numero del que deseamos calcular el factorial
-$numero = 7; // Puedes cambiar este numero al que desees calcular su factorial
+    // Verificar si el numero es divisible por algun otro numero que no sea el mismo y 1
+    for ($i = 2; $i <= sqrt($numero); $i++) {
+        if ($numero % $i == 0) {
+            return false;
+        }
+    }
 
-// Inicializar el factorial como 1
-$factorial = 1;
+    // Si no se encontraron divisores distintos de 1 y el propio numero, entonces es primo
+    return true;
+}
+// Definir el numero del que queremos verificar si es primo
+$numero = 17; // Puedes cambiar este numero al que desees verificar
 
-// Calcular el factorial usando un bucle for
-for ($i = 1; $i <= $numero; $i++) {
-    $factorial *= $i;
+// Llamar a la funcion esPrimo y mostrar el resultado por pantalla
+if (esPrimo($numero)) {
+    echo "$numero es un numero primo";
+} else {
+    echo "$numero no es un numero primo";
 }
 
-// Mostrar el factorial por pantalla
-echo "el factorial de $numero es: $factorial";
+
 ?>
